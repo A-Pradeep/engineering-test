ad30fe12916355682b6a30657c0fc047eb9ae55f
Pradeep A <sharmapradeep872@gmail.com>
2021-09-09 22:19:36 +0530

    Capture total attendance status. Display attendance count based on rollState as changed by user. Filter students based on rollState.

--- front-end/src/staff-app/components/active-roll-overlay/active-roll-overlay.component.tsx
+++ front-end/src/staff-app/components/active-roll-overlay/active-roll-overlay.component.tsx
@@ -3,16 +3,21 @@ import styled from "styled-components"
 import Button from "@material-ui/core/Button"
 import { BorderRadius, Spacing } from "shared/styles/styles"
 import { RollStateList } from "staff-app/components/roll-state/roll-state-list.component"
+import { studentData } from "staff-app/daily-care/home-board.page"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export type ActiveRollAction = "filter" | "exit"
 interface Props {
+  studentsData: studentData
+  totalStudentsCount: number
   isActive: boolean
+  loadState: boolean
   onItemClick: (action: ActiveRollAction, value?: string) => void
+  handleSortRoll: Function
 }
 
 export const ActiveRollOverlay: React.FC<Props> = (props) => {
-  const { isActive, onItemClick } = props
-
+  const { isActive, onItemClick, studentsData, totalStudentsCount, loadState, handleSortRoll } = props
   return (
     <S.Overlay isActive={isActive}>
       <S.Content>
@@ -20,18 +25,24 @@ export const ActiveRollOverlay: React.FC<Props> = (props) => {
         <div>
           <RollStateList
             stateList={[
-              { type: "all", count: 0 },
-              { type: "present", count: 0 },
-              { type: "late", count: 0 },
-              { type: "absent", count: 0 },
+              { type: "all", count: totalStudentsCount },
+              { type: "present", count: studentsData.present },
+              { type: "late", count: studentsData.late },
+              { type: "absent", count: studentsData.absent },
             ]}
+            sortRollType={handleSortRoll}
           />
           <div style={{ marginTop: Spacing.u6 }}>
             <Button color="inherit" onClick={() => onItemClick("exit")}>
               Exit
             </Button>
-            <Button color="inherit" style={{ marginLeft: Spacing.u2 }} onClick={() => onItemClick("exit")}>
-              Complete
+            <Button
+              color="inherit"
+              style={{ marginLeft: Spacing.u2, color: totalStudentsCount === 0 ? "slategray" : "white" }}
+              onClick={() => onItemClick("filter")}
+              disabled={totalStudentsCount === 0 ? true : false}
+            >
+              {loadState ? <FontAwesomeIcon icon="spinner" size="2x" spin /> : "Complete"}
             </Button>
           </div>
         </div>
