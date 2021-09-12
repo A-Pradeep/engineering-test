054ca7f465543f736900a20487e20f0e3b0fbd50
Pradeep A <sharmapradeep872@gmail.com>
2021-09-12 21:46:57 +0530

    Updated functions with operations

--- back-end/src/controller/group-controller.ts
+++ back-end/src/controller/group-controller.ts
@@ -1,45 +1,74 @@
 import { NextFunction, Request, Response } from "express"
+import { getRepository } from "typeorm"
+import { Group } from "../entity/group.entity"
+import { CreateGroup, UpdateGroup } from "../interface/group.interface"
 
 export class GroupController {
+  private groupRepository = getRepository(Group)
 
   async allGroups(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
+    // Task 1:
     // Return the list of all groups
+    return this.groupRepository.find()
   }
 
   async createGroup(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
+    // Task 1:
     // Add a Group
+    const { body: params } = request
+
+    const createGroup: CreateGroup = {
+      name: params.name,
+      number_of_weeks: params.number_of_weeks,
+      roll_states: params.roll_states,
+      incidents: params.incidents,
+      ltmt: params.ltmt,
+      student_count: params.student_count,
+    }
+    // initalise class
+    const group = new Group()
+    group.prepareToCreate(createGroup)
+
+    return this.groupRepository.save(group)
   }
 
   async updateGroup(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
+    // Task 1:
     // Update a Group
+    const { body: params } = request
+
+    const updateGroup: UpdateGroup = {
+      id: params.id,
+      name: params.name,
+      number_of_weeks: params.number_of_weeks,
+      roll_states: params.roll_states,
+      incidents: params.incidents,
+      ltmt: params.ltmt,
+      student_count: params.student_count,
+    }
+    // initalise class
+    const group = new Group()
+    group.prepareToUpdate(updateGroup)
+
+    return this.groupRepository.update(updateGroup.id, group)
   }
 
   async removeGroup(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
+    // Task 1:
     // Delete a Group
+    let groupToRemove = await this.groupRepository.findOne(request.params.id)
+    await this.groupRepository.remove(groupToRemove)
   }
 
   async getGroupStudents(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-        
+    // Task 1:
     // Return the list of Students that are in a Group
   }
 
-
   async runGroupFilters(request: Request, response: Response, next: NextFunction) {
     // Task 2:
-  
     // 1. Clear out the groups (delete all the students from the groups)
-
     // 2. For each group, query the student rolls to see which students match the filter for the group
-
     // 3. Add the list of students that match the filter to the group
   }
 }
